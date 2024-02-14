beb5c21fde2daa2ed62ec1414132dbce743ba358
valleXYZ <valentin.seehausen@gmail.com>
2024-02-14 15:17:09 +0100

    add tanstack query provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import { config } from './lib/wagmiConfig';
 import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>
 );
+
