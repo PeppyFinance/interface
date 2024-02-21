004b0f12f89ecfacf5342330b1c39bf41c2c3bde
valleXYZ <valentin.seehausen@gmail.com>
2024-02-21 16:39:52 +0100

    add query for positions

--- src/components/PositionList/index.tsx
+++ src/components/PositionList/index.tsx
@@ -1,24 +1,26 @@
 import { gql, useQuery } from 'urql';
+import { useAccount } from 'wagmi';
 
 const UserPositionsQuery = gql`
-  query UserPositions {
-    User {
+  query UserPositions($owner: String!) {
+    Position(where: { owner: { _eq: $owner } }) {
+      collateral
       id
-      positions {
-        id
-        collateral
-        direction
-        entryTimestamp
-      }
-      address
     }
   }
 `;
 
 export function PositionList() {
+  const { address, status } = useAccount();
+
   const [result] = useQuery({
     query: UserPositionsQuery,
+    variables: { owner: address },
   });
+
+  if (status !== 'connected') {
+    return <div>Connect your wallet to see your positions</div>;
+  }
 
   const { data, fetching, error } = result;
 
