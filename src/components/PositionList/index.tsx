888c0ceb10c7f55961a67bf0c38721dea9dfd559
nstylo <niklas@stylianou.info>
2024-03-08 15:24:00 -0700

    WIP: Add UI for positions

--- src/components/PositionList/index.tsx
+++ src/components/PositionList/index.tsx
@@ -1,28 +1,57 @@
-import { gql, useSubscription } from 'urql';
-import { formatEther } from 'viem';
+import { graphql } from '@/graphql/generated';
+import { GetPositionsSubscription } from '@/graphql/generated/graphql';
+import { useNavigate } from 'react-router-dom';
+import { useSubscription } from 'urql';
 import { useAccount } from 'wagmi';
+import { Card, CardContent, CardHeader } from '../ui/card';
+import { ScrollArea } from '../ui/scroll-area';
+import classNames from 'classnames';
 
-const newPositionsSubscription = gql`
-  subscription UserPositions($owner: String!) {
-    Position(where: { owner: { _eq: $owner } }) {
+const newPositionsSubscription = graphql(/* GraphQL */ `
+  subscription getPositions($owner: String!) {
+    Position(where: { owner: { address: { _eq: $owner } } }) {
       collateral
+      assets
       direction
       entryTimestamp
       id
     }
   }
-`;
+`);
+
+interface PositionProps {
+  collateral: bigint;
+  assets: bigint;
+  isLong: boolean;
+}
+
+const Position = ({ collateral, assets, isLong }: PositionProps) => {
+  return (
+    <Card className="my-6 bg-glass/20 backdrop-blur-md rounded-md ">
+      <CardHeader
+        className={classNames('rounded-t-md p-0', {
+          'bg-constructive/70': isLong,
+          'bg-destructive/70': !isLong,
+        })}
+      >
+        <p className="ml-4">{isLong ? 'long' : 'short'}</p>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+};
 
 export function PositionList() {
-  const { address, status } = useAccount();
+  const navigate = useNavigate();
+  const { address, isConnected } = useAccount();
 
-  const [result] = useSubscription({
+  const [result] = useSubscription<GetPositionsSubscription>({
     query: newPositionsSubscription,
     variables: { owner: address },
   });
 
-  if (status !== 'connected') {
-    return <div>Connect your wallet to see your positions</div>;
+  if (address === undefined || !isConnected) {
+    navigate('/');
   }
 
   const { data, fetching, error } = result;
@@ -30,17 +59,20 @@ export function PositionList() {
 
   if (fetching) return <div>Loading...</div>;
 
-  if (error) return <div>Error: {(error as BaseError).shortMessage || error.message || error}</div>;
-
   return (
-    <div>
-      <h1>Positions</h1>
-      <ul>
-        {data.Position.map(position => (
-          <li key={position.id}>{formatEther(position.collateral)} USDC</li>
-        ))}
-      </ul>
+    <div className="flex justify-center">
+      <ScrollArea className="w-[90%]">
+        {data
+          ? data.Position.map(position => (
+            <Position
+              key={position.id}
+              collateral={position.collateral}
+              assets={position.assets}
+              isLong={position.direction === '1'}
+            />
+          ))
+          : []}
+      </ScrollArea>
     </div>
   );
 }
-
