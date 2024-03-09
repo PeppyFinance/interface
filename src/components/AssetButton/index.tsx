27c519d37d24de1b98b6e31392a15a67c6852269
nstylo <niklas@stylianou.info>
2024-03-09 14:12:41 -0700

    Outsource Asset component

--- src/components/AssetButton/index.tsx
+++ src/components/AssetButton/index.tsx
@@ -19,41 +19,12 @@ import {
   DrawerTrigger,
 } from '../ui/drawer';
 import { ScrollArea } from '../ui/scroll-area';
-import { ReactNode, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { Market } from '@/types';
 import { useStore } from '@/store';
 import { formatPrice } from '@/lib/utils';
-
-interface Asset {
-  key: string;
-  value: string;
-}
-
-interface AssetProps {
-  children?: ReactNode | ReactNode[];
-  asset: Asset;
-  path: string;
-  onClick?: (asset: Asset) => void;
-}
-
-const Asset = (props: AssetProps) => {
-  return (
-    <div onClick={() => props.onClick && props.onClick(props.asset)} className="flex">
-      <div className="mr-2 p-2 flex items-center w-16 h-full">
-        <img
-          src={`/coins/${props.path}.svg`}
-          alt={props.path}
-          className="p-[1px] rounded-full w-16 border-2 border-foreground"
-        />
-      </div>
-      <div className="flex items-center text-xl font-bold">
-        {props.asset.value}
-        {props.children}
-      </div>
-    </div>
-  );
-};
+import { Asset } from '../Asset';
 
 const assets = [
   {
