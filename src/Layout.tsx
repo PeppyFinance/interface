f6d3c66dccdd8f665f372ff9d1372a992069101e
Tobias Liauw <tobias@liauw-media.de>
2024-04-14 09:25:28 +0200

    fixed merge error in layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -11,8 +11,6 @@ export const Layout = () => {
     <div className="tracking-widest h-screen flex flex-col bg-[url('/background.png')] bg-center bg-cover overflow-hidden">
       <Topbar />
       <div className="flex-auto overflow-auto w-full" id="content">
-      <Topbar />
-      <div className="flex-auto overflow-auto w-full" id="content">
         <Outlet />
       </div>
       <footer>
